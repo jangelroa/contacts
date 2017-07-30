da439ec1594d2756bd64c469ecd6ef353a08b1ed
jangelroa <jangelroa@gmail.com>
2017-07-29 23:17:44 -0700

    Code as Lesson 3.2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ListContacts from './ListContacts';
 
-class ContactList extends Component {
-  render() {
-    const people = this.props.contacts;
-    return (
-      <ol>
-        {people.map(contact => <li>{contact.name}</li>)}
-      </ol>
-    );
+const contacts = [
+  {
+    "id": "ryan",
+    "name": "Ryan Florence",
+    "email": "ryan@reacttraining.com",
+    "avatarURL": "http://localhost:5001/ryan.jpg"
+  },
+  {
+    "id": "michael",
+    "name": "Michael Jackson",
+    "email": "michael@reacttraining.com",
+    "avatarURL": "http://localhost:5001/michael.jpg"
+  },
+  {
+    "id": "tyler",
+    "name": "Tyler McGinnis",
+    "email": "tyler@reacttraining.com",
+    "avatarURL": "http://localhost:5001/tyler.jpg"
   }
-}
-
-
+];
 
 class App extends Component {
   render() {
     return (
       <div>
-        <ContactList contacts={[{ name: 'Michael' }, { name: 'Ryan' }, { name: 'Tyler' }]} />
-        <ContactList contacts={[{ name: 'Amanda' }, { name: 'Richard' }, { name: 'Geoff' }]} />
+        <ListContacts contacts={contacts} />
       </div>
     );
   }
